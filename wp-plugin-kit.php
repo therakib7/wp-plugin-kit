d6e3dc72fe08e1844c9ccec0a8e2862f379d17ab
Rakibul Hasan <therakib7>
2024-01-18 23:16:36 +0600

    Initial WP Plugin Kit

---
+++ wp-plugin-kit.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * WpPluginKit - A starter plugin for WordPress
+ *
+ * @package    Rakib - WpPluginKit
+ * @author     Rakib <therakib7@gmail.com>
+ * @link       https://therakib7.com
+ * @copyright  2023 Rakib
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Plugin Kit
+ * Plugin URI:        https://wordpress.org/plugins/wp-plugin-kit
+ * Description:       A starter plugin for WordPress
+ * Version:           0.1.0
+ * Author:            Rakib
+ * Author URI:        https://therakib7.com
+ * Requires at least: 5.8
+ * Requires PHP:      7.4
+ * Tested up to:      6.4
+ * Author:            Rakib
+ * Author URI:        https://therakib7.com
+ * Text Domain:       wp-plugin-kit
+ * Domain Path:       /languages
+  * License: GPL3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * Don't allow call the file directly
+ *
+ * @since 0.1.0
+ */
+defined( 'ABSPATH' ) || exit;
